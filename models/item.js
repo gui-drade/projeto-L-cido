d2c42347e9f0e5712aca1436c06cc5606b69761d
Guilherme de Andrade <guilhermeandradebarbo112@gmail.com>
2026-06-28 16:37:18 -0300

    alteração no data type
    alteração do dataType pra não dar erro

--- models/item.js
+++ models/item.js
@@ -1,25 +1,34 @@
-const {Data_Types} = require('sequelize');
+const {DataTypes} = require('sequelize');
 const sequelize = require('../config/bd');
 const Item = sequelize.define(
     'Item', {
     nome: {
-        type: Data_Types.STRING,
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    imagem: {
+        type: DataTypes.STRING,
         allowNull: false
     },
     preco: {
-        type: Data_Types.FLOAT,
+        type: DataTypes.INTEGER,
         allowNull: false
     },
     quantidade: {
-        type: Data_Types.INTEGER,
+        type: DataTypes.INTEGER,
         allowNull: false
     },
     categoria: {
-        type: Data_Types.STRING,
+        type: DataTypes.STRING,
         allowNull: false
     },
     descricao: {
-        type: Data_Types.STRING,
+        type: DataTypes.STRING,
     }},
     {
         tableName: 'Item',
