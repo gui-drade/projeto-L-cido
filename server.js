9bae2c94eab7dc023c01edddf2c8adc211171099
Guilherme de Andrade <guilhermeandradebarbo112@gmail.com>
2026-07-01 20:33:49 -0300

    Versão final da vitrine e do server.js

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const sequelize = require('./config/bd');
 const { Op } = require('sequelize');
 // Importando o modelo Item.
 const Item = require('./models/item');
+//Importando o modelo de estudante
+const Estudante = require('.models/estudante')
 
 app.engine('handlebars', exphbs.engine({defaultLayout: false}));
 app.set('view engine', 'handlebars');
@@ -104,4 +106,53 @@ app.post('/editar/:id', async(req,res) =>{
      where: {id:idItem}
     })
   res.redirect('/vitrine')
-})
+});
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/cadastro-aluno', (req, res) => {
+    res.render('cadastro-aluno');
+});
+
+app.get('/acoes-rapidas', (req, res) => {
+    res.render('acoes-rapidas');
+});
+
+app.post('/aluno/cadastrar', async (req, res) => {
+    await Estudante.create({ 
+        nome: req.body.nome, 
+        idade: req.body.idade || 20 
+    });
+    res.redirect('/gerenciar-alunos');
+});
+
+app.get('/gerenciar-alunos', async (req, res) => {
+    const termo = req.query.pesquisa || '';
+    
+    const estudantesDoBanco = await Estudante.findAll({
+        where: {
+            nome: { [Op.like]: `%${termo}%` }
+        }
+    });
+
+    const alunos = estudantesDoBanco.map(e => e.toJSON());
+    res.render('gerenciar-alunos', { alunos, pesquisa: termo });
+});
+
+app.post('/aluno/editar/:id', async (req, res) => {
+    await Estudante.update(
+        { nome: req.body.nome },
+        { where: { id: req.params.id } }
+    );
+    res.redirect('/gerenciar-alunos');
+});
+
+app.get('/aluno/deletar/:id', async (req, res) => {
+    await Estudante.destroy({ 
+        where: { id: req.params.id } 
+    });
+    res.redirect('/gerenciar-alunos');
+});
+
